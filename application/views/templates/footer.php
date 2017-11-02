cd5b9c41deb338c1e81ec9f13dcb26045bd7e17f
Renda <renda86@yahoo.com>
2017-11-02 08:11:13 +0700

    * tambah live chat

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -58,6 +58,20 @@
 		
 		<!-- Theme Initialization Files -->
 		<script src="<?php echo base_url('assets/js').'/theme.init.js'; ?>"></script>
-
+		
+		<!--Live chat-->
+		<!--Start of Tawk.to Script-->
+		<script type="text/javascript">
+			var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+			(function(){
+				var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+				s1.async=true;
+				s1.src='https://embed.tawk.to/59fa6e05bb0c3f433d4c6713/default';
+				s1.charset='UTF-8';
+				s1.setAttribute('crossorigin','*');
+				s0.parentNode.insertBefore(s1,s0);
+			})();
+		</script>
+		<!--End of Tawk.to Script-->
 	</body>
 </html>
